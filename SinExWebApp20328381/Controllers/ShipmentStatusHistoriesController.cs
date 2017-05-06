5cb8846bcb7e7684a510d705dafe402afabbc077
LIN Zizheng <zlinai@WIN2K.CS.UST.HK>
2017-05-06 20:53:27 +0800

    Fix a few bugs of tracking system

--- SinExWebApp20328381/Controllers/ShipmentStatusHistoriesController.cs
+++ SinExWebApp20328381/Controllers/ShipmentStatusHistoriesController.cs
@@ -52,7 +52,7 @@ namespace SinExWebApp20328381.Controllers
             {
                 return RedirectToAction("Search", new { errorMessage = "No such a waybill" });
             } else if (shipment.Status == "Saved" || shipment.Status == "Cancelled") {
-                return RedirectToAction("Search", new { erroMessage ="The waybill is not yet picked up or already cancelled"});
+                return RedirectToAction("Search", new { errorMessage = "The waybill is not yet picked up or already cancelled"});
             }
 
                 ViewData["WaybillNumber"] = shipment.WaybillId.ToString().PadLeft(12, '0'); ;
@@ -70,7 +70,7 @@ namespace SinExWebApp20328381.Controllers
                 List<Package> Packages = shipment.Packages.ToList();
                 ViewData["Packages"] = Packages;
 
-                 ViewData["Status_Informed"] = TempData["Status_Informed"];
+                 ViewData["Status_Inform"] = TempData["Status_Inform"];
                 //Retrieve shipmentStatusHistory
                 var statusQuery = from status in db.ShipmentStatusHistories
                                   where status.WaybillId == WaybillId
@@ -105,7 +105,7 @@ namespace SinExWebApp20328381.Controllers
             shipment.Packages = new Package[] { new Package() };
             
             
-            ViewData["Status_Informed"] = TempData["Status_Informed"];
+            ViewData["Status_Informed"] = TempData["Status_Inform"];
 
             ShipmentStatusHistory sta = new ShipmentStatusHistory();
             sta.Shipment = shipment;
@@ -135,7 +135,7 @@ namespace SinExWebApp20328381.Controllers
             TempData["Status_Inform"] = "";
             if (Status != "Confirmed" && Status != "PickedUp") {
                 TempData["Status_Inform"] = "Status of shipment can only be updated after it is confirmed and before it is delivered!";
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { WaybillId = WaybillId });
             }
             return View();
         }
@@ -151,13 +151,9 @@ namespace SinExWebApp20328381.Controllers
             //Compare the Date with current time
             DateTime statusDate = new DateTime(shipmentStatusHistory.Date.Year, shipmentStatusHistory.Date.Month,
                 shipmentStatusHistory.Date.Day, shipmentStatusHistory.Time.Value.Hour,shipmentStatusHistory.Time.Value.Minute,shipmentStatusHistory.Time.Value.Second);
-            if (statusDate > DateTime.Now) {
-                ModelState.AddModelError("","The specified date can not be set to be the time in the future!");
-                return View(shipmentStatusHistory);
-            }
 
             //Compare the Date with the previous time
-            /*var status_list = db.ShipmentStatusHistories.Select(s => s).ToList();
+           var status_list = db.ShipmentStatusHistories.Where(s => s.WaybillId == shipmentStatusHistory.WaybillId).ToList();
             DateTime pastMax = new DateTime(1900,01,01,01,01,01);
      
             foreach (var status in status_list) {
@@ -169,17 +165,30 @@ namespace SinExWebApp20328381.Controllers
             }
 
             if (statusDate <= pastMax) {
-                ModelState.AddModelError("", "The specified date is not valid!");
+                ModelState.AddModelError("", "The specified date is not valid! The added date should be later than the date and time of last status!");
+                ViewData["WaybillId"] = shipmentStatusHistory.WaybillId;
+                ViewData["Status"] = shipmentStatusHistory.Status;
                 return View(shipmentStatusHistory);
             }
-            */
+
 
             //Check whether the delivered information is entered while it is not delivered
-            if (shipmentStatusHistory.Status != "Delivered") {
-                if (shipmentStatusHistory.DeliveredPerson!=null || shipmentStatusHistory.DeliveredPlace!=null) {
+            if (shipmentStatusHistory.Status != "Delivered")
+            {
+                if (shipmentStatusHistory.DeliveredPerson != null || shipmentStatusHistory.DeliveredPlace != null)
+                {
                     ModelState.AddModelError("", "The Delivered person and delivered place field cannot be entered unless the shipment is delivered");
+                    ViewData["WaybillId"] = shipmentStatusHistory.WaybillId;
+                    ViewData["Status"] = shipmentStatusHistory.Status;
                     return View(shipmentStatusHistory);
                 }
+            }
+            else if(shipmentStatusHistory.DeliveredPerson == null || shipmentStatusHistory.DeliveredPlace == null)
+            {
+                ModelState.AddModelError("", "The Delivered person and delivered place field should be specified when the shipment is delivered");
+                ViewData["WaybillId"] = shipmentStatusHistory.WaybillId;
+                ViewData["Status"] = shipmentStatusHistory.Status;
+                return View(shipmentStatusHistory);
             }
 
             if (ModelState.IsValid)
